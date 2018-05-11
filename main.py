12fec30761b0d896f3a0bbe04c07f889ff61805c
dtran288 <dtran288@gmail.com>
2018-05-11 15:28:50 -0700

    Push Test

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     ##  Start Game
     print('Start TRIVIA')
-    print('Q1:  What is the meaning of life?')
+    print('Q1:  What is the meaning of cats?')
     print('Q2: Why do hot dogs come in packages of 8, but buns in packages of 6?')
     print('Q3: How much wood can a woodchuck chuck if a woodchuck could chuck wood?')
 
