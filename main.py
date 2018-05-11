b79db4ca784702272675627bd1f1b23e25486213
dtran288 <dtran288@gmail.com>
2018-05-11 15:12:26 -0700

    First Draft

---
+++ main.py
@@ -0,0 +1,23 @@
+#First Line
+
+from __future__ import print_function
+
+if __name__ == '__main__':
+    print("GAME STARTS")
+
+    ## Introduction
+    print('Hey sucker, here\'s the game')
+
+
+    ## Get Player's Name
+    player_name = raw_input('Who you?')
+
+
+    ##  Start Game
+    print('Start TRIVIA')
+    print('Q1:  What is the meaning of life?')
+    print('Q2: Why do hot dogs come in packages of 8, but buns in packages of 6?')
+    print('Q3: How much wood can a woodchuck chuck if a woodchuck could chuck wood?')
+
+    ##  End Game
+    print('Be GONE,%s !' %player_name)
