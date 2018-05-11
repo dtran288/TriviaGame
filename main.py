8536cd672b4522e6b4873067d25d0fe72ebd350a
dtran288 <dtran288@gmail.com>
2018-05-11 16:39:06 -0700

    class Question added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #First Line
 
 from __future__ import print_function
+from question import populate_question_database
 
 if __name__ == '__main__':
     print("GAME STARTS")
@@ -10,14 +11,16 @@ if __name__ == '__main__':
 
 
     ## Get Player's Name
-    player_name = raw_input('Who you?')
+    # player_name = raw_input('Who you?')
+    player_name = "Dany"
+    num_questions = 2
 
 
     ##  Start Game
     print('Start TRIVIA')
-    print('Q1:  What is the meaning of cats?')
-    print('Q2: Why do hot dogs come in packages of 8, but buns in packages of 6?')
-    print('Q3: How much wood can a woodchuck chuck if a woodchuck could chuck wood?')
+    list_of_questions = populate_question_database(num_questions)
+    for question in list_of_questions:
+        question.display_question()
 
     ##  End Game
     print('Be GONE,%s !' %player_name)
--- question.py
+++ question.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+import urllib, json
+
 class Question:
 
     def __init__(self, question, correct_answer, incorrect_answers):
@@ -19,7 +22,31 @@ class Question:
         print(self.correct_answer)
         print(self.incorrect_answers)
 
+def populate_question_database(num_questions):
+    """
+    Fetches data from the url, turns it into Question objects and returns it as a list
+    User can specify the number of questions through the argument num_questions
+    """
+    assert type(num_questions) is int
+    assert num_questions < 11
+    assert num_questions > 1
 
+    # Get question database API from the url
+    url = "https://opentdb.com/api.php?amount=" + str(num_questions) + "&type=multiple"
+    response = urllib.urlopen(url)
+    # This will be a dictionary with results being key that points to the list of dictionary of questions
+    question_db = json.loads(response.read())
+
+    list_of_questions = [] # initialize the empty list of questions
+    # Read through all questions and create a Question object for each of them
+    for question_dict in question_db['results']:
+        question = question_dict['question']
+        correct_answer = question_dict['correct_answer']
+        incorrect_answers = question_dict['incorrect_answers']
+
+        list_of_questions.append(Question(question, correct_answer, incorrect_answers))
+
+    return list_of_questions
 
 
 
